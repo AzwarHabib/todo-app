586d9a23adca1d25c744d714c227afc7f2c9c2c0
AzwarHabib <Azwar.dev.01@gmail.com>
2024-09-19 15:48:12 +0500

    Update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,16 +1,12 @@
-const awsconfig = {
-    aws_project_region: 'ap-south-1',
-    aws_appsync_graphqlEndpoint: 'https://4vwo5oltjvgtfeqt3rbavnreja.appsync-api.ap-south-1.amazonaws.com/graphql',
-    aws_appsync_region: 'ap-south-1',
-    aws_appsync_authenticationType: 'apiKey', // or 'AMAZON_COGNITO_USER_POOLS', etc.
-    aws_appsync_apiKey: 'da2-hgwpfwdpbzgkjk3bx2hvamzj3u',
 
+  const awsconfig = {
+    aws_project_region: process.env.VUE_APP_AWS_PROJECT_REGION,
+    aws_appsync_graphqlEndpoint: process.env.VUE_APP_AWS_APPSYNC_GRAPHQL_ENDPOINT,
+    aws_appsync_region: process.env.VUE_APP_AWS_APPSYNC_REGION,
+    aws_appsync_authenticationType: process.env.VUE_APP_AWS_APPSYNC_AUTHENTICATION_TYPE,
+    aws_appsync_apiKey: process.env.VUE_APP_AWS_APPSYNC_API_KEY,
   };
+  
 
-//   endpoint: 'https://4vwo5oltjvgtfeqt3rbavnreja.appsync-api.ap-south-1.amazonaws.com/graphql',
-//           region: 'ap-south-1',
-//           defaultAuthMode: 'apiKey',
-//           apiKey: 'da2-hgwpfwdpbzgkjk3bx2hvamzj3u'
   
   export default awsconfig;
-  
